384b87502ae5b4e6eba8ada85a59b61fa06742aa
Artuwok1986 <artem.vlasenko@gmail.com>
2016-11-24 17:51:07 +0200

    - added polymorphic method to find first  element in array

--- src/main/scala-2.12/func_prog_in_scala/chapter_02/Monomorph.scala
+++ src/main/scala-2.12/func_prog_in_scala/chapter_02/Monomorph.scala
@@ -1,20 +1,32 @@
 package func_prog_in_scala.chapter_02
 
-//example of monomorphic function
+import scala.annotation.tailrec
+
+
 object Monomorph {
 
   def main(args: Array[String]): Unit = {
     println(findFirst(Array("ss", "zz", "yy"), "yy"))
   }
 
+  //example of monomorphic function
   def findFirst(ss: Array[String], key: String) = {
-
+    @tailrec
     def loop(i: Int): Int = {
-
       if (i >= ss.length) -1
       else if (ss(i) == key) i
-
       else loop(i + 1)
+    }
+    loop(0)
+  }
+
+  // Example of polymorphic function
+  def findFirstUniversal[A](as: Array[A], p: A => Boolean): Int = {
+    @tailrec
+    def loop(i: Int): Int = {
+      if (i >= as.length) -1
+      else if (p(as(i))) i
+      else loop(i+1)
     }
     loop(0)
   }
