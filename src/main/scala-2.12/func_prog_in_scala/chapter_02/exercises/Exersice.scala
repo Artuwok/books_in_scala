4cb26b43afa29cda537d99bdeb1ac23ce9fd5d12
Artuwok1986 <artem.vlasenko@gmail.com>
2016-11-30 13:54:37 +0200

    - added uncurry function

--- src/main/scala-2.12/func_prog_in_scala/chapter_02/exercises/Exersice.scala
+++ src/main/scala-2.12/func_prog_in_scala/chapter_02/exercises/Exersice.scala
@@ -8,6 +8,34 @@ object Exersice {
   //implementation that compiles. Write this implementation.
 
   def curry[A, B, C](f: (A, B) => C): A => (B => C) =
-    a => b => f(a,b)
+    a => b => f(a, b)
+
+  // Explanation of above function
+  def curryInDetails[A,B,C](f: (A, B) => C): A => (B => C) = {
+    def aToBToC(a: A): B => C = {
+      def bToC(b: B): C = {
+        f(a,b)
+      }
+      bToC
+    }
+    aToBToC
+  }
+
+  // EXERCISE 2.4
+  //Implement uncurry , which reverses the transformation of curry . Note that since =>
+  // associates to the right, A => (B => C) can be written as A => B => C .
+
+   def uncurry[A,B,C](f: A=>B=>C): (A,B)=>C = {
+    (a: A, b: B) => f(a)(b)
+  }
+
+  //EXERCISE 2.5
+  //Implement the higher-order function that composes two functions.
+  //*compose создает новую функцию, которая является объединением других функций f(g(x))
+  def compose[A,B,C](f: B => C, g: A=>B):A =>C ={
+    a => f(g(a))
+
+
+  }
 
 }
