2d468ead7be2b2c53d6ddd3dcd37ea07a748da3e
Artuwok1986 <artem.vlasenko@gmail.com>
2017-01-07 15:54:19 +0200

    added ex09

---
+++ src/main/scala/scala_for_impatient/chapter_01/Ex_09.scala
@@ -0,0 +1,7 @@
+package scala_for_impatient.chapter_01
+
+//How do you get the first character of a string in Scala? The last character?
+object Ex_09 extends App {
+  println("Artem".charAt(0))
+  println("Artem".last)
+}
