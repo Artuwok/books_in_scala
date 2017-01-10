b764243e90631e25d469133d7206975934e0036a
Artuwok1986 <artem.vlasenko@gmail.com>
2017-01-10 21:23:59 +0200

    added ex-04 chapter_02

--- src/main/scala/scala_for_impatient/chapter_02/Ex_04.scala
+++ src/main/scala/scala_for_impatient/chapter_02/Ex_04.scala
@@ -4,7 +4,7 @@ package scala_for_impatient.chapter_02
 // for (int i = 10; i >= 0; i--) System.out.println(i);
 object Ex_04 extends App {
 
-  for (i <- 10 to 0) {
+  for (i <- 10 to 0 by -1) {
     println(i)
   }
 }
