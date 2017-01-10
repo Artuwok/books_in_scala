54d541f9d761cf72f13973450977ad4448a97326
Artuwok1986 <artem.vlasenko@gmail.com>
2017-01-10 21:15:15 +0200

    added ex-04 chapter_02

---
+++ src/main/scala/scala_for_impatient/chapter_02/Ex_04.scala
@@ -0,0 +1,10 @@
+package scala_for_impatient.chapter_02
+
+// Write a Scala equivalent for the Java loop
+// for (int i = 10; i >= 0; i--) System.out.println(i);
+object Ex_04 extends App {
+
+  for (i <- 10 to 0) {
+    println(i)
+  }
+}
