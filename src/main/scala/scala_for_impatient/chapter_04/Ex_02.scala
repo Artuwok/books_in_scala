4cf56ca861202ca72c13b689284afd4c7c6d302d
Artuwok1986 <artem.vlasenko@gmail.com>
2017-01-21 12:23:48 +0200

    updated ex_02 chapter 04

--- src/main/scala/scala_for_impatient/chapter_04/Ex_02.scala
+++ src/main/scala/scala_for_impatient/chapter_04/Ex_02.scala
@@ -14,21 +14,29 @@ import scala.io.Source
 
 object Ex_02 extends App {
 
-  // Mutable javaway solution
+  // Java way
   var wordsMap = new scala.collection.mutable.HashMap[String, Int]
-  val in = new Scanner(new java.io.File("/home/artemvlasenko/Desktop/myfile.txt"))
+  val in = new Scanner(new java.io.File("/home/art/Desktop/qwe.txt"))
   while (in.hasNext) {
     val word = in.next()
     val count = wordsMap.getOrElse(word, 0)
     wordsMap(word) = count + 1
     in.next()
   }
-  wordsMap.foreach(p => println("word is: " + p._1, "count is: " + p._2))
+  wordsMap.foreach(p => println("word is: " + p._1, " count is: " + p._2))
 
+  println("---Scala way---")
   // Scala way
-  val imut
-  val file = Source.fromFile("/home/artemvlasenko/Desktop/myfile.txt")
+  val secondWordsMap = new scala.collection.mutable.HashMap[String, Int]
+  val file = Source.fromFile("/home/art/Desktop/qwe.txt")
   val tokens: Array[String] = file.mkString.split("\\s+") // s+ one or many white spases
 
-  for
+  for (word <- tokens) yield {
+    if (!secondWordsMap.contains(word)) {
+      secondWordsMap(word) = 1
+    }
+    else secondWordsMap(word) = secondWordsMap(word) + 1
+  }
+
+  secondWordsMap.foreach(p => println("word is: " + p._1 + " count is: " + p._2))
 }
