58cb6ddba640b8b5ca3e3c9f9be1fc49f457f8b4
Artuwok1986 <artem.vlasenko@gmail.com>
2017-01-26 23:02:52 +0200

    added ex_07  chapter 08

---
+++ src/main/scala/scala_for_impatient/chapter_08/Ex_07/Square.scala
@@ -0,0 +1,16 @@
+package scala_for_impatient.chapter_08.Ex_07
+
+// Provide a class Square that extends java.awt.Rectangle and has three constructors: one that
+// constructs a square with a given corner point and width, one that constructs a square with corner
+// (0, 0) and a given width, and one that constructs a square with corner (0, 0) and width 0 .
+class Square(val cornerPoint: Double, val width: Int) extends java.awt.Rectangle {
+
+  def this(width: Int) = {
+    this(0.0, width)
+  }
+
+  def this() = {
+    this(0.0, 0)
+  }
+
+}
